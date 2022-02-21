0234b89c4280cd368c6be172354d4c3b1b88cb56
CarlosGA30 <garcia.arriaga.jose.carlos@gmail.com>
2022-02-20 22:45:09 -0600

    añadiendo carrito volador

--- assets/js/carrito-compras.js
+++ assets/js/carrito-compras.js
@@ -1,180 +1,37 @@
 let $boton_pagar=document.querySelector('#pagar');
+let $sub_total=document.querySelector('#sub-total');
 let $total=document.querySelector('#total');
+let $envio=document.querySelector('#envio');
 let $meme_cabeza=document.getElementById('meme-cabeza');
 let $meme_img=document.getElementById('meme-img');
 let $label_text_tarjeta=document.querySelector("#radio-tarjeta").querySelector("label");
 let $rd_btn_paypal=document.getElementById('paypal');
 let $rd_btn_tarjeta=document.getElementById('tarjeta-credito');
 let $tarjeta=document.querySelector('#tarjeta');
-let $cvv;
-let $num_tarjeta;
-let $vigencia;
-let span_cvv;
-let span_num_tarjeta;
-let $btn_reset_tarjeta;
-let $btn_submit_tarjeta;
-let span_vigencia;
-
-function crearTarjeta(){
-    $tarjeta.innerHTML=`<h2>💳</h2>
-    <div>
-     <input id="num-tarjeta" class="text-principal m1" type="text" placeholder="numero de tarjeta">
-    </div>
-    <input id="cvv" class="text-principal m-1" type="text" placeholder="cvv">
-    <input type="text" class="text-principal m-1" name="mes" id="vigencia" placeholder="mm/yy">
-    <br>
-    <input type="submit" class="btn btn-success rounded" value="guardar">
-    <input type="reset" class="btn btn-danger rounded ">`;
-    $tarjeta.classList.add("bg-warning");
-    $cvv=$tarjeta.querySelector("#cvv");
-    $num_tarjeta=$tarjeta.querySelector("#num-tarjeta");
-    $vigencia = $tarjeta.querySelector('#vigencia');
-    span_cvv=document.createElement("span");
-    span_num_tarjeta=document.createElement("span");
-    $btn_reset_tarjeta=$tarjeta.querySelector('input[type="reset"]');
-    $btn_submit_tarjeta=$tarjeta.querySelector('input[type="submit"]');
-    span_vigencia=document.createElement("span");
-    $cvv.insertAdjacentElement("afterend", span_cvv);
-    $num_tarjeta.insertAdjacentElement("afterend", span_num_tarjeta);
-    $vigencia.insertAdjacentElement("afterend", span_vigencia);
-    $cvv.value=tarjeta.cvv;
-    $num_tarjeta.value=tarjeta.num_tarjeta;
-    $vigencia.value=tarjeta.vigencia;
-
-    $vigencia.addEventListener("keypress",(e)=>{
-        if(span_vigencia.className=="error-label")
-            $vigencia.value="";
-        let numero=/[0-9]/;
-        e.preventDefault();
-        if(numero.test(e.key) && $vigencia.value.length<5){
-            if($vigencia.value.length==2)
-                $vigencia.value+="/";
-            tarjeta.vigencia=$vigencia.value+e.key;
-            if(tarjeta.vigencia.length==5){
-                if(tarjeta.isCorectoVigencia()){
-                    span_vigencia.className="correcto-label";
-                    span_vigencia.innerText="✅";
-                }
-
-            }
-            
-        if(tarjeta.vigencia.length==2)
-            tarjeta.vigencia+="/";
-        $vigencia.value=tarjeta.vigencia;
-                
-        }
-        if(!tarjeta.isCorectoVigencia()){
-            span_vigencia.className="";
-            span_vigencia.innerText="";
-            if(tarjeta.vigencia.length==5){
-                span_vigencia.className="error-label";
-                span_vigencia.innerText="❌";
-            }
-            $vigencia.insertAdjacentElement("afterend", span_vigencia)
+let $productos=document.querySelector('#productos');
+let carrito_total=0;
+dibujaCarrito();
+document.addEventListener("click",(e)=>{
+    if(e.target.parentElement.parentNode.parentNode.parentNode.id=="productos"){
 
     }
-        
-    });
+    if(e.target.classList.contains('borrar-Articulo') ) {
+        remover(e.target.getAttribute('data-id'));
+   }else if(e.target.classList.contains('vaciar')){
+        articulosCarrito=[];
+        //carritoHTML();
+   }else if(e.target.classList.contains('+')){
+        sumarArticulo(e.target.getAttribute('data-id'),1);
+        //carritoHTML();
+   }else if(e.target.classList.contains('-')){
+        restarArticulo(e.target.getAttribute('data-id'));
+   }
 
-    $cvv.addEventListener("keypress",(e)=>{
-        let numero=/[0-9]/;
-        if(numero.test(e.key) && $cvv.value.length<3){
-        tarjeta.cvv=$cvv.value+e.key;
-        if(tarjeta.isCorectoCVV()){
-                span_cvv.className="correcto-label";
-                span_cvv.innerText="✅";
-                $cvv.insertAdjacentElement("afterend", span_cvv);
-        }
-        
-        }else{
-            
-            e.preventDefault();
-        }
-        if(!tarjeta.isCorectoCVV()){
-            span_cvv.className="";
-            span_cvv.innerText="";
-            $cvv.insertAdjacentElement("afterend", span_cvv)
-    }
-
-        
-    });
-    $num_tarjeta.addEventListener("keypress",(e)=>{
-        if(span_num_tarjeta.className=="error-label")
-            $num_tarjeta.value="";
-        let numero=/[0-9]/;
-        e.preventDefault();
-        if(numero.test(e.key) && $num_tarjeta.value.length<19){
-            if($num_tarjeta.value.length==4 || $num_tarjeta.value.length==9 || $num_tarjeta.value.length==14)
-                $num_tarjeta.value+="-";
-            tarjeta.num_tarjeta=$num_tarjeta.value+e.key;
-            if(tarjeta.isCorectoNum_tarjeta()){
-                span_num_tarjeta.className="correcto-label";
-                span_num_tarjeta.innerText="✅";
-                $num_tarjeta.insertAdjacentElement("afterend", span_num_tarjeta);
-            }
-            
-            if(tarjeta.num_tarjeta.length==4 || tarjeta.num_tarjeta.length==9 || tarjeta.num_tarjeta.length==14)
-                $num_tarjeta.value=tarjeta.num_tarjeta+"-";
-            $num_tarjeta.value=tarjeta.num_tarjeta;
-                
-        }
-        
-        if(!tarjeta.isCorectoNum_tarjeta()){
-            span_num_tarjeta.className="";
-            span_num_tarjeta.innerText="";
-            if(tarjeta.num_tarjeta.length==19){
-                span_num_tarjeta.className="error-label";
-                span_num_tarjeta.innerText="❌";
-            }
-            $num_tarjeta.insertAdjacentElement("afterend", span_num_tarjeta);
-        }
-    });
-
-    $btn_reset_tarjeta.addEventListener("click",(e)=>{
-        span_cvv.innerText="";
-        span_num_tarjeta.innerText="";
-        span_vigencia.innerText="";
-        span_cvv.className="";
-        span_num_tarjeta.className="";
-        span_vigencia.className="";
-        tarjeta.cvv="";
-        tarjeta.vigencia="";
-        tarjeta.num_tarjeta="";
+   carritoHTML();
+    dibujaCarrito();
     
-    });
-    $btn_submit_tarjeta.addEventListener("click",(e)=>{
-        e.stopImmediatePropagation();
-        e.preventDefault();
-        tarjeta.cvv=$cvv.value;
-        tarjeta.num_tarjeta=$num_tarjeta.value;
-        tarjeta.vigencia=$vigencia.value;
-        if(tarjeta.isCompleta()){
-            $rd_btn_tarjeta.checked=true;
-            quitarTarjeta();
-            $label_text_tarjeta.innerText="********"+tarjeta.num_tarjeta.substring(15);
-            dibuja_meme();
-        }else{
-
-            if(!tarjeta.isCorectoCVV()){
-                span_cvv.className="error-label";
-                span_cvv.innerText="❌";
-            }
-            if(!tarjeta.isCorectoNum_tarjeta()){
-                span_num_tarjeta.className="error-label";
-                span_num_tarjeta.innerText="❌";
-            }
-            if(!tarjeta.isCorectoVigencia()){
-                span_vigencia.className="error-label";
-                span_vigencia.innerText="❌";
-            }
-        }
-    });
-
-}
-function quitarTarjeta(){
-    $tarjeta.innerHTML=``;
-    $tarjeta.classList.remove("bg-warning");   
-}
+   
+});
 let medio_de_pago={
     medio:"",
     tipo:"",
@@ -184,12 +41,12 @@ let tarjeta={
     num_tarjeta:"",
     cvv:"",
     vigencia:"",
-    isCorectoCVV(){ 
+    isCorectoCVV(tar){ 
         let patern=/[0-9]{3}/;
-        return patern.test(this.cvv);
+        return patern.test(tar.cvv);
     },
-    isCorectoNum_tarjeta(){ 
-        let secciones=this.num_tarjeta.split("-");
+    isCorectoNum_tarjeta(tar){ 
+        let secciones=tar.num_tarjeta.split("-");
         let correcto=(secciones.length==4);
         let i=0;
         while(correcto && i<secciones.length){
@@ -198,34 +55,136 @@ let tarjeta={
         }
         return correcto;
     },
-    isCorectoVigencia(){
+    isCorectoVigencia(tar){
         let patern=/[0-9]{2}\/[0-9]{2}/;
         let fecha=new Date();
-        let f_anio=this.vigencia.split("/");
+        let f_anio=tar.vigencia.split("/");
         let mes=parseInt(f_anio[0]);
         let anio=parseInt(f_anio[1]);
-        return (mes>=fecha.getMonth() && anio>=(fecha.getFullYear()-2000) && mes<=12 && patern.test(this.vigencia));
+        return (mes>=fecha.getMonth() && anio>=(fecha.getFullYear()-2000) && mes<=12 && patern.test(tar.vigencia));
     },
-    isCompleta(){return (this.isCorectoNum_tarjeta() && this.isCorectoCVV() && this.isCorectoVigencia())}
+    isCompleta(tar){
+        return (tar.isCorectoNum_tarjeta(tar) && tar.isCorectoCVV(tar) && tar.isCorectoVigencia(tar))
+    }
 
 }
+function crearTarjeta(){
+    $tarjeta.innerHTML=`<h2>💳</h2>
+    <div class="text-left p-1">
+     <input id="num-tarjeta" class="h6 m1" type="text" placeholder="numero de tarjeta">
+     <i class="fas"></i>
+    </div>
+    <div class="text-left">
+    <span>
+    <input id="cvv" class="h6 m-1" type="text" placeholder="cvv">
+    <i class="fas"></i>
+    </span>
+    <span>
+    <input type="text" class="h6 m-1" name="mes" id="vigencia" placeholder="mm/yy">
+    <i class="fas"></i>
+    </span>
+    </div>
+    <input type="submit" class="btn bg-dark text-white rounded p-1" value="guardar">
+    <input type="reset" class="btn bg-danger text-white rounded p-1">`;
+    $tarjeta.classList.add("bg-white");
+    ($tarjeta.querySelector("#cvv")).setAttribute('maxlength','3');
+    ($tarjeta.querySelector("#num-tarjeta")).setAttribute('maxlength','19');
+    ($tarjeta.querySelector('#vigencia')).setAttribute("maxlength",'5');
+    $tarjeta.addEventListener("keypress",e=>{
+        let numero=/[0-9]/;
+        if(!numero.test(e.key)){
+            e.preventDefault(); 
+        }else{
+            verificarDato(e);
+        }
+    });
+    $tarjeta.addEventListener("keyup",e=>{
+        let numero=/[0-9]/;
+        if(!numero.test(e.key)){
+            e.preventDefault(); 
+        }else{
+            verificarDato(e);
+        }
+    });
+    function verificarDato(e){
+        
+        let dato=e.target;
+        let lugar=dato.parentElement
+        switch (dato.id) {
+            case 'cvv':
+                tarjeta.cvv=dato.value;
+                dato.value=tarjeta.cvv;
+                cambiarClase(lugar,tarjeta.isCorectoCVV);
+                break;
+            case 'num-tarjeta':
+                tarjeta.num_tarjeta=agregarCaracter(dato.value,[5,10,15],"-");
+                dato.value=tarjeta.num_tarjeta;
+                cambiarClase(lugar,tarjeta.isCorectoNum_tarjeta);
+                break;
+            case 'vigencia':
+                tarjeta.vigencia=agregarCaracter(dato.value,[3],"/");
+                dato.value=tarjeta.vigencia;
+                cambiarClase(lugar,tarjeta.isCorectoVigencia);
+                break;
+        }
+        console.log(tarjeta);
+    }
+    function agregarCaracter(texto,posiciones,simbolo){
+        let resultado=(posiciones.includes(texto.length+1))?texto+simbolo:texto;
+        return resultado;
+        
+    }
+    function cambiarClase(dato,funcionValidacion){
+        let lugar=dato.querySelector('.fas');
+        lugar.classList.add((funcionValidacion(tarjeta))?'fa-check-circle':'fa-times-circle');
+        lugar.classList.remove((!funcionValidacion(tarjeta))?'fa-check-circle':'fa-times-circle');
+    }
+   
+    $tarjeta.addEventListener("click",(e)=>{
+        let tar=e.target;
+        console.log(tar);
+        if(tar.type=="submit"){
+            e.preventDefault();
+            console.log(tarjeta)
+            console.log(tarjeta.isCompleta(tarjeta));
+            if(tarjeta.isCompleta(tarjeta)){
+                $rd_btn_tarjeta.checked=true;
+                quitarTarjeta();
+                $label_text_tarjeta.innerText="********"+tarjeta.num_tarjeta.substring(15);
+                dibuja_meme();
+            }else{
+                console.log(($tarjeta.querySelector('#cvv')).parentElement);
+                cambiarClase(($tarjeta.querySelector('#cvv')).parentElement,tarjeta.isCorectoCVV);
+                cambiarClase(($tarjeta.querySelector('#num-tarjeta')).parentElement,tarjeta.isCorectoNum_tarjeta);
+                cambiarClase(($tarjeta.querySelector('#vigencia')).parentElement,tarjeta.isCorectoVigencia);
+                
+            }
 
+        }
+        
+    });
+
+}
+function quitarTarjeta(){
+    $tarjeta.innerHTML=``;
+    $tarjeta.classList.remove("bg-warning");   
+}
 function dibuja_meme(){
-    if(parseFloat($total.innerText)==0.0){
+    if(carrito_total==0.0){
         $meme_cabeza.innerText='Tu en la parte de pagos sin productos';
         $meme_img.src="../assets/img/elBuenJhon.png";
       
-    }else if(parseFloat($total.innerText)<1000){
-        $meme_cabeza.innerText='Los que no les importa pagar gatos de envio';
+    }else if(carrito_total<3000){
+        $meme_cabeza.innerText='Los que pagan gatos de envio';
         $meme_img.src="../assets/img/lobo.png";
        
     }else {
-        $meme_cabeza.innerText='When compras mas de 1000 para no pagar envio';
+        $meme_cabeza.innerText='When compras mas de 3000 para no pagar envio';
         $meme_img.src="../assets/img/hombreDeNegocios.png";
     }
 }
 dibuja_meme();
-$rd_btn_paypal.addEventListener("click",(e)=>{
+document.getElementById('paypal').addEventListener("click",(e)=>{
     medio_de_pago.medio="paypal";
     quitarTarjeta();
     dibuja_meme();
@@ -243,7 +202,7 @@ $boton_pagar.addEventListener("click",(e)=>{
         e.stopImmediatePropagation();
         e.preventDefault();
     }else if(!medio_de_pago.isActivo()){
-        $meme_cabeza.innerText='Lorem sin saber con que vas a pagar';
+        $meme_cabeza.innerHTML=' <span class="logo"></span> sin saber con que vas a pagar';
         $meme_img.src="../assets/img/elBuenJhon.png";
         e.stopImmediatePropagation();
         e.preventDefault();
@@ -251,7 +210,47 @@ $boton_pagar.addEventListener("click",(e)=>{
         
 
     }
-    
+    ponerLogo();
    
 });
 
+function dibujaCarrito(){
+    limpiaProductos();
+    let table=$productos.querySelector('table');
+    let total=0;
+
+    articulosCarrito.forEach(Articulo => {
+        let subTot=parseInt(Articulo.cantidad)*parseFloat(Articulo.precio.split(" ")[2]);
+        const row = document.createElement('tr');
+        const img=document.createElement('td');
+        img.innerHTML=`<td><img src="${Articulo.imagen}" width=70 height=80></td>`;
+        row.appendChild(img);
+        row.innerHTML += `
+             
+             <td class="h6" style="width:150px">${Articulo.titulo}</td>
+             <td class="h6">$${subTot}</td>
+             <td style="width:70px;height:80px; display: flex; align-items: center;">
+             <button class="-" style="border-radius:50%; transform: scale(0.6);" data-id="${Articulo.id}">➖</button
+             ><span class="h6">${Articulo.cantidad}</span><button
+              data-id="${Articulo.id}" class="+" style="border-radius:50%; transform: scale(0.6);">➕</button></td>
+             <td style="color:white;padding:2px">
+                  <a href="#" class="borrar-Articulo h6" style=" transform: scale(2);" data-id="${Articulo.id}">X</a>
+             </td>
+        `;
+        total+=subTot;
+        table.appendChild(row);
+   });
+   $sub_total.innerText='$'+total;
+   carrito_total=total;
+   let envio=(total>1000)?((total<3000)?total*0.15:0):100;
+   $envio.innerText='$'+envio;
+   $total.innerText='$'+(total+envio);
+   dibuja_meme();
+}
+function ponerLogo(){
+    document.querySelectorAll('.logo').forEach(e=>
+        e.innerHTML=`Cheems<span style="color:#f02121f3">&</span>Mems`);
+}
+function limpiaProductos(){
+    $productos.querySelector('table').innerHTML="";
+}
