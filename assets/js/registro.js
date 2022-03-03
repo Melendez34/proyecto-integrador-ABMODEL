f101623e4deb3a2a9fb456c196838637710be2c3
Omar Melendez <omar.mel.lopez@gmail.com>
2022-03-03 14:36:54 -0600

    Ajuste de js y link de terminos y condiciones

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -15,99 +15,6 @@ const campos = {
   telefono: false,
 };
 
-const validarFormulario = (e) => {
-  switch (e.target.name) {
-    case "usuario":
-      validarCampo(expresiones.usuario, e.target, "usuario");
-      break;
-    case "password":
-      validarCampo(expresiones.password, e.target, "password");
-      validarPassword2();
-      break;
-    case "password2":
-      validarPassword2();
-      break;
-    case "correo":
-      validarCampo(expresiones.correo, e.target, "correo");
-      break;
-    case "telefono":
-      validarCampo(expresiones.telefono, e.target, "telefono");
-      break;
-  }
-};
-
-const validarCampo = (expresion, input, campo) => {
-  if (expresion.test(input.value)) {
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.remove("formulario__grupo-incorrecto");
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.add("formulario__grupo-correcto");
-    document.querySelector(`#grupo__${campo} i`).classList.add("fa-check-circle");
-    document.querySelector(`#grupo__${campo} i`).classList.remove("fa-times-circle");
-    document
-      .querySelector(`#grupo__${campo} .formulario__input-error`)
-      .classList.remove("formulario__input-error-activo");
-    campos[campo] = true;
-  } else {
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.add("formulario__grupo-incorrecto");
-    document
-      .getElementById(`grupo__${campo}`)
-      .classList.remove("formulario__grupo-correcto");
-    document.querySelector(`#grupo__${campo} i`).classList.add("fa-times-circle");
-    document.querySelector(`#grupo__${campo} i`).classList.remove("fa-check-circle");
-    document
-      .querySelector(`#grupo__${campo} .formulario__input-error`)
-      .classList.add("formulario__input-error-activo");
-    campos[campo] = false;
-  }
-};
-
-const validarPassword2 = () => {
-  const inputPassword1 = document.getElementById("password");
-  const inputPassword2 = document.getElementById("password2");
-
-  if (inputPassword1.value !== inputPassword2.value) {
-    document
-      .getElementById(`grupo__password2`)
-      .classList.add("formulario__grupo-incorrecto");
-    document
-      .getElementById(`grupo__password2`)
-      .classList.remove("formulario__grupo-correcto");
-    document.querySelector(`#grupo__password2 i`).classList.add("fa-times-circle");
-    document
-      .querySelector(`#grupo__password2 i`)
-      .classList.remove("fa-check-circle");
-    document
-      .querySelector(`#grupo__password2 .formulario__input-error`)
-      .classList.add("formulario__input-error-activo");
-    campos["password"] = false;
-  } else {
-    document
-      .getElementById(`grupo__password2`)
-      .classList.remove("formulario__grupo-incorrecto");
-    document
-      .getElementById(`grupo__password2`)
-      .classList.add("formulario__grupo-correcto");
-    document
-      .querySelector(`#grupo__password2 i`)
-      .classList.remove("fa-times-circle");
-    document.querySelector(`#grupo__password2 i`).classList.add("fa-check-circle");
-    document
-      .querySelector(`#grupo__password2 .formulario__input-error`)
-      .classList.remove("formulario__input-error-activo");
-    campos["password"] = true;
-  }
-};
-
-inputs.forEach((input) => {
-  input.addEventListener("keyup", validarFormulario);
-  input.addEventListener("blur", validarFormulario);
-});
-
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -124,10 +31,10 @@ formulario.addEventListener("submit", (e) => {
     fetch("http://localhost:8080/usuarios", {
       method: "POST",
       body: JSON.stringify({
-        nombre: "Omar",
-        contrasenia: 1234,
-        email: "omar@mail.com",
-        telefono: 55554444,
+        usuario: "Omar",
+        contrasenia: "1234",
+        email: "123@123.com",
+        telefono: "44332222",
       }),
       headers: {
         "Content-type": "application/json",
