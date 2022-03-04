df39c655a073efea6a2ff8b3bf0265ccf4fca97d
CarlosGA30 <garcia.arriaga.jose.carlos@gmail.com>
2022-03-03 20:15:59 -0600

    aql

---
+++ sql/cheems_MEMS.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO productos (valor,nombre,cantidad_inventario, precio) 
+VALUES
+('v_popup','Camisa Patricio tengo fe',150,150),
+('v_popup2','Chamarra de Gato.',300,300),
+('v_popup3','Chamarra "Me da ansiedad"',342,342),
+('v_popup4','Chamarra "No puede ser"',230,230),
+('v_popup5','Gorra de Dodge',90,90),
+('v_popup6','Taza de sobrevivo por pura ansiedad.',200,200),
+('v_popup7','Taza de "ia stoy arto"',172,172),
+('v_popup8','Gorra de toy triste',192,192),
+('v_popup9','Camisa de cheems',172,172),
+('k_popup','Hoodie cheems color blanco.',400,400),
+('k_popup2','Hoodie rana color negro.',400,400),
+('k_popup3','Hoodie Troll, color negro.',400,400),
+('k_popup4','Playera Cheems, color blanco.',150,150),
+('k_popup5','Playera cheems, color negro.',150,150),
+('k_popup6','Playera confusion, color blanco.',150,150),
+('k_popup7','Taza ¡oh no!.',40,40),
+('k_popup8','cubreboca varios colores.',50,50),
+('k_popup9','gorra "dijiste que...", color negra.',200,200);
